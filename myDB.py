a944e03a05e1b7427e1330ea53d33196d1052e8d
Michal1323 <57396719+Michal1323@users.noreply.github.com>
2020-12-06 18:01:48 +0000

    Updated the MyDB.py file to have read and write.
    Added the read and write fields to the database and updated the myDB.py file to include the write and read functions.

---
+++ myDB.py
@@ -0,0 +1,138 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from .__init__ import db
+
+
+class userTable(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(4096))
+    user_id = db.Column(db.Integer)
+    authkey = db.Column(db.String(4096))
+    login = db.Column(db.Integer)
+    read_access = db.Column(db.Integer)
+    write_access = db.Column(db.Integer)
+
+    def __init__(self, name, user_id, authkey, login, read_access, write_access ):
+        self.name = name
+        self.user_id = user_id
+        self.authkey = authkey
+        self.login = login
+        self.read_access = read_access
+        self.write_access = write_access
+
+
+def delete_all():
+    try:
+        db.session.query(UserTable).delete()
+        db.session.commit()
+        print("Delete all Done")
+    except Exception as e:
+        print("Failed " + str(e))
+        db.session.rollback()
+
+
+def get_user_row_if_exists(user_id):
+    get_user_row = userTable.query.filter_by(user_id=user_id).first()
+    if get_user_row != None:
+        return get_user_row
+    else:
+        print("User does not exist")
+        return False
+
+
+def add_user_and_login(name, user_id):
+    row = get_user_row_if_exists(user_id)
+    if row != False:
+        row.login = 1
+        db.session().commit
+    else:
+        print("Adding user" + name)
+        new_user = userTable(name, user_id, None, 1)
+        db.session.add(new_user)
+        db.session.commit()
+    print("User" + name + "login added")
+
+
+def bool_to_int(v):
+    if 'true' in str(v):
+        return
+
+
+def add_user_permission(user_id, read, write):
+    row = get_user_row_if_exists(user_id)
+    if row:
+        row.read_access = bool_to_int(read)
+        row.write_access = bool_to_int(write)
+        db.session.commit()
+
+
+def user_logout(user_id):
+    row = get_user_row_if_exists(user_id)
+    if row:
+        row.authkey = authkey
+        db.session.commit()
+        print("User " + row.name + "authkey added")
+
+
+def get_authkey(user_id):
+    row = get_user_row_if_exists(user_id)
+    if row:
+        return row.authkey
+    else:
+        print("User with id" + user_id + " doesnt exist")
+
+
+def get_user_access(user_id):
+    row = get_user_row_if_exists(user_id)
+    if row:
+        get_user_row = UserTable.query.filter_by(user_id=user_id).first()
+        read = get_user_row.read_access
+        if read == 1:
+            read = True
+        else:
+           read = False
+    return read, write
+
+
+def view_all():
+    row = userTable.query.all()
+    for n in range(0, len(row)):
+        print(str(row[n].id) + " | " + row[n].name + " | " + str(row[n].user_id) + " | " + str(row[n].authkey) + " | " +
+              str(row[n].login) + "|" + str(row[n].read_access) + " | " + str(row[n].write_access))
+
+
+def get_all_logged_in_users():
+    row = userTable.query.filter_by(login=1).all()
+    online_user_record = {"user_record": []}
+    print("LoggedIn Users:")
+    for n in range(0, len(row)):
+        if row[n].read_access:
+            read = "checked"
+        else:
+            read = "unchecked"
+        if row[n].write_access:
+            write = "checked"
+        else:
+            write = "unchecked"
+        online_user_record["user_record"].append([row[n].name, row[n].user_id, read, write])
+        print(str(row[n].id) + " | " + row[n].name + " | " + str(row[n].user_id) + " | " + str(row[n].authkey) + " | " +
+              str(row[n].login) + "|" + str(row[n].read_access) + " | " + str(row[n].write_access))
+    return online_user_record
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
